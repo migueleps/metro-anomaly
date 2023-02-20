98e4ea3bf0ba81cfbbd1bb158817738a9db62930
migueleps <miguel.silva@postgrad.manchester.ac.uk>
2023-02-20 16:05:30 +0000

    corrected loss for discriminator

--- AAE_offline_only_train.py
+++ AAE_offline_only_train.py
@@ -50,7 +50,7 @@ def train_discriminator(optimizer, train_tensor, random_latent_space, epoch, arg
     loss_random_term = th.log(discriminator_random)
     loss_real_term = th.log(1-discriminator_real)
 
-    loss = args.WAE_regularization_term * -th.mean(loss_real_term + loss_random_term)
+    loss = args.WAE_regularization_term * th.mean(loss_real_term + loss_random_term)
 
     #with open(args.loss_logger("WAE_discriminator", epoch), "a") as gradient_file:
     #    gradient_file.write(f"Discriminator pre-sigmoid real latent\n")
@@ -65,7 +65,7 @@ def train_discriminator(optimizer, train_tensor, random_latent_space, epoch, arg
     #    gradient_file.write(f"{str(loss)}\n")
     #    gradient_file.write("\n")
 
-    loss.backward()
+    loss.backward(th.Tensor([-1]))
 
     nn.utils.clip_grad_norm_(args.discriminator.parameters(), 1)
 
